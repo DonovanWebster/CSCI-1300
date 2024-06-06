75fed70b6372883dc3cbb07e6842ca7c2f0e9ea1
angusbeef17 <dwebster751@outlook.com>
2024-06-06 10:05:27 -0600

    finished question 5

---
+++ homework/homework-1/question5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int knuts, sickles, galleons;
+    int sickleRemainder, galleonsRemainder;
+
+    cout << "Enter the number of knuts:" << endl;
+    cin >> knuts;
+
+    galleons = knuts / (17 * 29);
+    galleonsRemainder = knuts % (17 * 29);
+    sickles = galleonsRemainder / 29;
+    sickleRemainder = galleonsRemainder % 29;
+    knuts = sickleRemainder;
+
+    cout << galleons << " Galleon(s) " << sickles << " Sickle(s) " << knuts << " Knut(s)" << endl;
+
+    return 0;
+
+}
