ecdd733c7a3f033a3bd1e4333c82318c0ddcf787
angusbeef17 <dwebster751@outlook.com>
2024-06-11 09:55:31 -0600

    finished question 2

---
+++ homework/homework-2/question2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int carouselMinHeight = 30, teacupsMinHeight = 36, infernoMinHeight = 45;
+    string allowed = "Yes, you are allowed to go on that ride!\n";
+    string notAllowed = "Sorry, you cannot go on that ride.\n";
+    bool tallEnough = false;
+
+    double height;
+    char ride;
+
+    cout << "How tall are you in inches?" << endl;
+    cin >> height;
+    
+    if (height <= 0.0) {
+        cout << "Please enter a valid input." << endl;
+        return 1;
+    }
+
+    cout << "What ride would you like to take(c, t, or i)?" << endl;
+    cin >> ride;
+    
+    switch(ride) {
+        case 'c':
+            if (height >= carouselMinHeight) {
+                tallEnough = true;
+            }
+            break;
+        case 't':
+            if (height >= teacupsMinHeight) {
+                tallEnough = true;
+            }
+            break;
+        case 'i':
+            if (height >= infernoMinHeight) {
+                tallEnough = true;
+            }
+            break;
+        default: 
+            cout << "Please enter a valid input." << endl;
+            return 1;
+    }
+
+    if (!tallEnough) {
+        cout << notAllowed;
+        return 1;
+    }
+
+    cout << allowed;
+
+    return 0;
+
+}
