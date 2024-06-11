8a5266eb7c9f92d4e1b4cb25411307062718752e
angusbeef17 <dwebster751@outlook.com>
2024-06-11 10:17:30 -0600

    finished question 4

---
+++ homework/homework-2/question4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double price1, price2, price3;
+    bool increasing = false, decreasing = false;
+
+    cout << "Enter stock prices over last 3 weeks" << endl;
+    cin >> price1 >> price2 >> price3;
+
+    if (price1 <= 0 || price2 <= 0 || price3 <= 0) {
+        cout << "Please enter a valid input." << endl;
+        return 1;
+    }
+
+    if (price1 > price2 && price2 > price3) {
+        decreasing = true;
+    }
+    if (price1 < price2 && price2 < price3) {
+        increasing = true;
+    }
+
+    if (increasing) {
+        cout << "The value is rising!" << endl;
+        return 0;
+    }
+    if (decreasing) {
+        cout << "The value is tanking." << endl;
+        return 0;
+    }
+
+    cout << "Neither" << endl;
+
+    return 0;
+
+}
