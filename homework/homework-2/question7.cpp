45c2f1fd92593fa7f6b66942dbe9b630fe518174
angusbeef17 <dwebster751@outlook.com>
2024-06-11 11:25:51 -0600

    finished question 7

---
+++ homework/homework-2/question7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num, sum = 0;
+
+    cout << "Enter a positive integer:" << endl;
+    cin >> num;
+    if (num <= 0) {
+        cout << "Invalid input" << endl;
+        return 1;
+    }
+
+    while (num % 3 != 0) {
+        num--;
+    }
+
+    while (num > 0) {
+        sum += num;
+        num -= 3;
+    }
+
+    cout << "Sum: " << sum << endl;
+    
+    return 0;
+
+}
