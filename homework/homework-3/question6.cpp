38325ac0d8b368dd8c24920f0367bd1ae94f9c62
angusbeef17 <dwebster751@outlook.com>
2024-06-17 17:18:07 -0600

    finished question 6

--- homework/homework-3/question6.cpp
+++ homework/homework-3/question6.cpp
@@ -2,9 +2,35 @@
 
 #include <iostream>
 
+using namespace std;
+
+void print(int n, int l) {
+    for (int i = 0; i < n; i++) {
+        cout << "*";
+    }
+    for (int i = 0; i < (l-2*n); i++) {
+        cout << " ";
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+
 int main() {
 
+    cout << "Enter length:" << endl;
+    int length;
+    cin >> length;
 
+    int width = (2 * length) + 1;
+
+    for (int i = length; i > 0; i--) {
+        print(i, width);
+    }
+    for (int i = length - (length - 2); i < length + 1; i++) {
+        print(i, width);
+    }
 
     return 0;
 
