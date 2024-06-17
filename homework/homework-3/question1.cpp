d9de30e491c50e8d37124d080d70e3145200cf39
angusbeef17 <dwebster751@outlook.com>
2024-06-17 13:50:49 -0600

    finished question 1

--- homework/homework-3/question1.cpp
+++ homework/homework-3/question1.cpp
@@ -1,10 +1,30 @@
 // Homework 3 - Problem 1
 
 #include <iostream>
+#include <cctype>
+
+using namespace std;
 
 int main() {
 
+    cout << "Enter the string:" << endl;
 
+    string input, output = "";
+    getline(cin, input);
+
+    for (int i = 0; i < input.length(); i++) {
+        if (isalpha(input[i])) {
+            if (islower(input[i])) {
+                output += toupper(input[i]);
+            } else {
+                output += tolower(input[i]);
+            }
+        } else {
+            output += input[i];
+        }
+    }
+
+    cout << output;
 
     return 0;
 
