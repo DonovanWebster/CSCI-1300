66c5e49b210f941e79edb8ba76d5fc24f3815d23
angusbeef17 <dwebster751@outlook.com>
2024-06-17 17:08:16 -0600

    finished question 5

--- homework/homework-3/question5.cpp
+++ homework/homework-3/question5.cpp
@@ -2,9 +2,43 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
+    string alpha = "zyxwvutsrqponmlkjihgfedcba";
 
+    cout << "Enter the height:" << endl;
+    int height;
+    cin >> height;
+    if (height <= 0) {
+        cout << "Invalid input." << endl;
+        return 1;
+    }
+
+    int numChars = 0;
+    int j = height;
+    for (int i = 0; i < height; i++) {
+        numChars += j;
+        j--;
+    }
+
+    int repeats = numChars / alpha.length();
+    int remainder = numChars % alpha.length();
+
+    string out = "";
+
+    for (int i = 0; i < repeats; i++) {
+        out += alpha;
+    }
+    out += alpha.substr(0, remainder);
+
+    int start = 0;
+    for (int i = 0; i < height; i++) {
+        string sub = out.substr(start, height - i);
+        cout << out.substr(start, height - i) << endl;
+        start += height - i;
+    }
 
     return 0;
 
