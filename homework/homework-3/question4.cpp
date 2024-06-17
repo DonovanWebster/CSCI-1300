a87e97a46ce91c1f1084dac64a28c4c2e8bf0e59
angusbeef17 <dwebster751@outlook.com>
2024-06-17 16:11:59 -0600

    finished question 4

--- homework/homework-3/question4.cpp
+++ homework/homework-3/question4.cpp
@@ -2,9 +2,24 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
+    cout << "Enter the search string:" << endl;
+    string str, sub_str;
+    cin >> str;
+    cout << "Enter the substring to be searched:" << endl;
+    cin >> sub_str;
 
+    int count = 0;
+    for (int i = 0; i < str.length() - sub_str.length(); i++) {
+        if (str.substr(i, sub_str.length()) == sub_str) {
+            count++;
+        }
+    }
+
+    cout << "Number of occurrences: " << count << endl;
 
     return 0;
 
