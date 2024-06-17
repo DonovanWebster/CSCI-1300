f2bf85f11bca12b92f74e4f35b37b87632b0ce3f
angusbeef17 <dwebster751@outlook.com>
2024-06-17 14:16:31 -0600

    finished question 2

--- homework/homework-3/question2.cpp
+++ homework/homework-3/question2.cpp
@@ -1,10 +1,36 @@
 // Homework 3 - Problem 2
 
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long int getNextNumInSequence(long int n) {
+    long int num;
+    if (n % 2 == 0) {
+        num = floor(sqrt(n));
+    } else {
+        num = floor(pow(sqrt(n), 3));
+    }
+    return num;
+}
 
 int main() {
 
+    long int num;
+    cout << "Enter a positive integer:" << endl;
+    cin >> num;
+    if (num <= 0) {
+        cout << "Invalid input." << endl;
+        return 1;
+    }
 
+    while (num > 1) {
+        cout << num << endl;
+        num = getNextNumInSequence(num);
+    }
+
+    cout << num;
 
     return 0;
 
