d14597dffc2b84b8c794f1441064f2a387187acd
angusbeef17 <dwebster751@outlook.com>
2024-06-19 20:50:24 -0600

    finished question 7

---
+++ recitations/recitation-3/question7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int sum = 0;
+    for(int i=0; i < x; i++)
+    {
+        sum += x;
+    }
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
