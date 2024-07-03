c09948eded7c9b3e07b563505dc83035382bf0d4
angusbeef17 <dwebster751@outlook.com>
2024-07-03 11:45:25 -0600

    finished question 5

---
+++ recitations/recitation-5/question5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N = 5;
+	string colors[] = {"red", "blue", "yellow", "green"};
+
+	//printing all the colors
+	for (int i = 0; i < N - 1; i++){
+		cout << "My favorite color is " << colors[i] << endl;
+	}
+return 0;
+}
