76138f847cb9fa2eb1c7db366629ff5a9708e56b
angusbeef17 <dwebster751@outlook.com>
2024-07-03 11:41:50 -0600

    finished question 4

---
+++ recitations/recitation-5/question4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int temps[3] = {50, 53, 49};
+cout << "The most recently recorded temperature of the day is " << temps[2] << " degrees Fahrenheit." << endl;
+return 0;
+}
