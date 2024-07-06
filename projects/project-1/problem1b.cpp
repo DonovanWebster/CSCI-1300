065fa9bd48493ef1e582abf213219cc5e76c15ca
angusbeef17 <dwebster751@outlook.com>
2024-07-06 00:36:19 -0600

    finished 1b

---
+++ projects/project-1/problem1b.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+void printFileHeader(ofstream &file, int width, int height) {
+    file << "P2" << endl;
+    file << width << " " << height << endl;
+    file << 255 << endl;
+}
+
+bool isInCircle(int x, int y) {
+    return sqrt(pow(x, 2) + pow(y, 2)) < 30;
+}
+
+int main() {
+
+    int width = 100;
+    int height = 100;
+
+    ofstream outFile("../images/problem1b.pgm");
+    printFileHeader(outFile, width, height);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int x = j - (width/2);
+            int y = i - (height/2);
+
+            if (isInCircle(x, y)) {
+                outFile << 255 << " ";
+            } else {
+                outFile << 0 << " ";
+            }
+        }
+        outFile << endl;
+    }
+
+    return 0;
+
+}
